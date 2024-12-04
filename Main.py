c278e4d1971a452abf1a31565375f917388556b0
Mravacardo <matthewheathcote@icloud.com>
2024-12-04 16:05:28 +0000

    Main.py

--- Main.py
+++ Main.py
@@ -16,4 +16,34 @@ screen.blit(cheetah,(150,100))
 screen.blit(eagle,(150,200))
 screen.blit(horse,(150,300))
 screen.blit(meerkat,(150,400))
-font
+font=pygame.font.SysFont("Times New Roman",36)
+text=font.render("eagle",True,(0,0,0))
+text1=font.render("meerkat",True,(0,0,0))
+text2=font.render("cheetah",True,(0,0,0))
+text3=font.render("horse",True,(0,0,0))
+screen.blit(text,(350,100))
+screen.blit(text1,(350,200))
+screen.blit(text2,(350,300))
+screen.blit(text3,(350,400))
+pygame.display.update()
+r=randint(0,255)
+g=randint(0,255)
+b=randint(0,255)
+clr=(r,g,b)
+while 1:
+
+    event=pygame.event.poll()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+
+            pygame.quit()
+            exit(0)
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        pos = pygame.mouse.get_pos()
+        pygame.draw.circle(screen,((clr)),(pos), 20, 0)
+        pygame.display.update()
+    elif event.type == pygame.MOUSEBUTTONUP:
+        pos2 = pygame.mouse.get_pos()
+        pygame.draw.line(screen,((clr)),(pos),(pos2),5)
+        pygame.draw.circle(screen,((clr)),(pos2), 20, 0)
+        pygame.display.update()            
