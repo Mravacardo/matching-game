27b6a7517b3f79091a9c84be9001956f88ce6511
Mravacardo <matthewheathcote@icloud.com>
2024-12-04 16:23:27 +0000

    Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import pygame
 from random import randint
-pygame.init
+pygame.init()
 screen = pygame.display.set_mode((600,600))
 screen.fill((255,255,255))
 pygame.display.update()
@@ -8,10 +8,10 @@ cheetah=pygame.image.load("cheetah.jpg")
 eagle=pygame.image.load("eagle.jpg")
 horse=pygame.image.load("horse.jpg")
 meerkat=pygame.image.load("meerkat.jpg")
-cheetah=pygame.transform.scale(cheetah(75,75))
-eagle=pygame.transform.scale(eagle(75,75))
-horse=pygame.transform.scale(horse(75,75))
-meerkat=pygame.transform.scale(meerkat(75,75))
+cheetah=pygame.transform.scale(cheetah,(75,75))
+eagle=pygame.transform.scale(eagle,(75,75))
+horse=pygame.transform.scale(horse,(75,75))
+meerkat=pygame.transform.scale(meerkat,(75,75))
 screen.blit(cheetah,(150,100))
 screen.blit(eagle,(150,200))
 screen.blit(horse,(150,300))
